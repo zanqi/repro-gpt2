dd9d1b454279db72d730fde1516175c95d1dd5a5
Zanqi <zanqi.liang@gmail.com>
2025-03-28 10:37:50 -0700

    from_pretrained

--- train_gpt2.py
+++ train_gpt2.py
@@ -23,22 +23,86 @@ class GPT(nn.Module):
         self.transformer = nn.ModuleDict(
             dict(
                 wte=nn.Embedding(config.vocab_size, config.n_embd),
-                wpe=nn.Embedding(config.vocab_size, config.n_embd),
-                h=nn.ModuleList([Block(config) for _ in config.n_layer]),
+                wpe=nn.Embedding(config.block_size, config.n_embd),
+                h=nn.ModuleList([Block(config) for _ in range(config.n_layer)]),
                 ln_f=nn.LayerNorm(config.n_embd),
             )
         )
 
         self.lm_head = nn.Linear(config.n_embd, config.vocab_size, bias=False)
 
+    def forward(self, idx):
+        B, T = idx.size()
+        assert T <= self.config.block_size
+
+        pos = torch.arange(0, T, dtype=torch.long, device=idx.device)
+        pos_emb = self.transformer.wpe(pos) # (T, n_embd)
+        tok_emb = self.transformer.wte(idx) # (B, T, n_embd)
+        x = pos_emb + tok_emb
+
+        for block in self.transformer.h:
+            x = block(x)
+        
+        x = self.transformer.ln_f(x)
+        logits = self.lm_head(x)
+        return logits
+
     @classmethod
     def from_pretrained(cls, model_type):
         assert model_type in {"gpt2", "gpt2-medium", "gpt2-large", "gpt2-xl"}
+        from transformers import GPT2LMHeadModel
+
+        print(f"loading weights from pretrained gpt: {model_type}")
+        config_args = {
+            # 124M params
+            "gpt2": dict(n_layer=12, n_head=12, n_embd=768),
+            # 350M params
+            "gpt2-medium": dict(n_layer=24, n_head=16, n_embd=1024),
+            # 774M params
+            "gpt2-large": dict(n_layer=36, n_head=20, n_embd=1280),
+            # 1558M params
+            "gpt2-xl": dict(n_layer=48, n_head=25, n_embd=1600),
+        }[model_type]
+        config_args["vocab_size"] = 50257
+        config_args["block_size"] = 1024
+        config = GPTConfig(**config_args)
+        model = GPT(config)
+        sd = model.state_dict()
+        sd_keys = sd.keys()
+        sd_keys = [k for k in sd_keys if not k.endswith(".attn.bias")]
+
+        model_hf = GPT2LMHeadModel.from_pretrained(model_type)
+        sd_hf = model_hf.state_dict()
+        sd_keys_hf = sd_hf.keys()
+        sd_keys_hf = [k for k in sd_keys_hf if not k.endswith(".attn.bias")]
+        sd_keys_hf = [k for k in sd_keys_hf if not k.endswith(".attn.masked_bias")]
+        assert len(sd_keys) == len(
+            sd_keys_hf
+        ), f"mismatch keys: {len(sd_keys)} != {len(sd_keys_hf)}"
+        transposed = [
+            "attn.c_attn.weight",
+            "attn.c_proj.weight",
+            "mlp.c_fc.weight",
+            "mlp.c_proj.weight",
+        ]
+        for k in sd_keys_hf:
+            if any(k.endswith(w) for w in transposed):
+                assert sd_hf[k].shape[::-1] == sd[k].shape
+                with torch.no_grad():
+                    sd[k].copy_(sd_hf[k].t())
+            else:
+                # vanilla copy over the other parameters
+                assert (
+                    sd_hf[k].shape == sd[k].shape
+                ), f"[{k}]: {sd[k].shape} != {sd_hf[k].shape}"
+                with torch.no_grad():
+                    sd[k].copy_(sd_hf[k])
+        return model
 
 
 class Block(nn.Module):
 
-    def __init__(self, *args, config: GPTConfig):
+    def __init__(self, config: GPTConfig):
         super().__init__()
         self.ln_1 = nn.LayerNorm(config.n_embd)
         self.attn = CausalSelfAttention(config)
@@ -72,6 +136,9 @@ class CausalSelfAttention(nn.Module):
         super().__init__()
         assert config.n_embd % config.n_head == 0
 
+        # Linear's weight shape is (out_features, in_features)
+        # Think of it as a matrix multiply an input column vector
+        # on the left: M @ input
         self.c_attn = nn.Linear(config.n_embd, 3 * config.n_embd)
         self.c_proj = nn.Linear(config.n_embd, config.n_embd)
         self.n_head = config.n_head
@@ -102,3 +169,18 @@ class CausalSelfAttention(nn.Module):
         y = att @ v  # (B, nh, T, T) x (B, nh, T, hs) -> (B, nh, T, hs)
         y = self.c_proj(y)
         return y
+
+
+# ---------------------------------------------------------------
+num_return_sequences = 5
+max_length = 30
+
+model = GPT.from_pretrained("gpt2")
+# Only dropout and batchNorm has different bahavior between
+# eval and train mode. Our model doesn't have those, so this
+# line is probably not needed.
+model.eval()
+model.to('cuda')
+
+import tiktoken
+enc = tiktoken.
